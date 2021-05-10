491705132fbc37191b52e6f7804b984533c5e55d
Francisco Miguel Peralta <franmper@MacBook-Air-de-Francisco.local>
2021-05-10 10:07:17 -0300

    add typescript support to serverless

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,11 @@
-'use strict';
-
-module.exports.hello = async (event) => {
+export const hello = async (
+  event: () => { statusCode: string; body: string }
+) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
