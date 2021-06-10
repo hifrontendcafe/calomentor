1704d46670c64670c983359c19b6c2d555584e91
Francisco Miguel Peralta <franmper@MacBook-Air-de-Francisco.local>
2021-06-10 20:30:01 -0300

    fix

--- packages/server/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const TABLE_NAME_USER: string = "user";
-export const TABLE_NAME_TIME_SLOT: string = "time-slot";
-export const TABLE_NAME_MENTORSHIP: string = "mentorship";
--- packages/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { Handler, Context, Callback } from "aws-lambda";
-import { testService } from "./services/adminService";
-
-export const test: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback
-) =>
-  await testService(event, context, callback);
