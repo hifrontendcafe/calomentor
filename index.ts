120cdde7500defe6c21fcc739c21514445bc8574
hadita <anahigaetan@hotmail.com>
2021-07-03 20:59:16 -0300

    Agrego primer approach de crud de usuarios

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { Handler, Context, Callback } from "aws-lambda";
 import { testService } from "./services/testService";
-import { createUserService, activateMentorService } from "./services/mentorService";
-
+import { createUserService, activateMentorService, getUsersService, getUserByIdService, deleteUserByIdService, updateUserByIdService } from "./services/userService";
 
 export const test: Handler = async (
   event: any,
@@ -20,6 +19,30 @@ export const createUser: Handler = (
   context: Context,
   callback: Callback<any>
 ) => createUserService(event, context, callback);
+
+export const getUsers: Handler = (
+  event: any,
+  context: Context,
+  callback: Callback<any>
+) => getUsersService(event, context, callback);
+
+export const getUserById: Handler = (
+  event: any,
+  context: Context,
+  callback: Callback<any>
+) => getUserByIdService(event, context, callback);
+
+export const deleteUserById: Handler = (
+  event: any,
+  context: Context,
+  callback: Callback<any>
+) => deleteUserByIdService(event, context, callback);
+
+export const updateUserById: Handler = (
+  event: any,
+  context: Context,
+  callback: Callback<any>
+) => updateUserByIdService(event, context, callback);
 
 export const mentorshipConfirmation: Handler = (
   event: any,
