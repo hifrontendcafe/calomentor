cddd01b189616d0625c3750a9189a8ca9409a606
hadita <anahigaetan@hotmail.com>
2021-09-07 15:53:25 -0300

    Agreglo los endpoints de user y mando fix para la validación de booleanos

--- utils/validations.js
+++ utils/validations.js
@@ -1,7 +1,4 @@
-import { throwResponse } from "./throwResponse";
-
 export const createAndUpdateUserValidations = (
-  callback,
   discord_username,
   full_name,
   email,
@@ -12,43 +9,38 @@ export const createAndUpdateUserValidations = (
 ) => {
   let errorMessage = "";
 
-  if (!discord_username || typeof discord_username !== "string" ) {
-    errorMessage = "Bad Request: discord_username is required or it's not a string.";
-    throwResponse(callback, errorMessage, 400);
+  if (!discord_username || typeof discord_username !== "string") {
+    return errorMessage =
+      "Bad Request: discord_username is required or it's not a string.";
   }
 
-  if (!full_name || typeof full_name !== "string" ) {
-    errorMessage = "Bad Request: full_name is required or it's not a string.";
-    throwResponse(callback, errorMessage, 400);
+  if (!full_name || typeof full_name !== "string") {
+    return errorMessage = "Bad Request: full_name is required or it's not a string.";
   }
 
-  if (!email || typeof email !== "string" ) {
-    errorMessage = "Bad Request: email is required or it's not a string.";
-    throwResponse(callback, errorMessage, 400);
+  if (!email || typeof email !== "string") {
+    return errorMessage = "Bad Request: email is required or it's not a string.";
   }
 
-  if (!url_photo || typeof url_photo !== "string" ) {
-    errorMessage = "Bad Request: url_photo is required or it's not a string.";
-    throwResponse(callback, errorMessage, 400);
+  if (!url_photo || typeof url_photo !== "string") {
+    return errorMessage = "Bad Request: url_photo is required or it's not a string.";
   }
 
   if (!role || !role.length || !Array.isArray(role)) {
-    errorMessage = "Bad Request: role is required or it's not an array.";
-    throwResponse(callback, errorMessage, 400);
+    return errorMessage = "Bad Request: role is required or it's not an array.";
   }
 
   if (
-    !links ||
     Array.isArray(links) ||
     typeof links !== "object" ||
     Object.keys(links).length === 0
   ) {
-    errorMessage = "Bad Request: links is required or it's not an object.";
-    throwResponse(callback, errorMessage, 400);
+    return errorMessage = "Bad Request: links is required or it's not an object.";
   }
 
   if (!skills || !skills.length || !Array.isArray(skills)) {
-    errorMessage = "Bad Request: skills is required or it's not an array.";
-    throwResponse(callback, errorMessage, 400);
+    return errorMessage = "Bad Request: skills is required or it's not an array.";
   }
+
+  return errorMessage;
 };
