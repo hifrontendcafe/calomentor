92c7af332997f25a9de77147546438c89f33af5e
Santiago Santana <santiago.santana@naturalth.it>
2021-11-09 00:38:42 -0300

    refactor: add types

--- repository/timeSlot.ts
+++ repository/timeSlot.ts
@@ -1,8 +1,9 @@
 import { TABLE_NAME_TIME_SLOT } from "../constants";
 import { get } from "../utils/dynamoDb";
+import { TimeSlot } from "../types";
 
 export function getTimeSlotById(id) {
-  return get({
+  return get<TimeSlot>({
     TableName: TABLE_NAME_TIME_SLOT,
     Key: { id },
   });
