e47af71799f1163dbcbeb450d045c746f23be649
Santiago Santana <santana.santiago@gmail.com>
2021-11-27 21:23:26 -0300

    style: improve nodemailer types

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,22 +1,23 @@
 import { createTransport } from "nodemailer";
+import { TransportOptions } from "nodemailer";
 
-/* tslint:disable */
+const config: TransportOptions = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.CALOMENTOR_MAIL,
+    clientId: process.env.CALOMENTOR_MAIL_CLIENT_ID,
+    clientSecret: process.env.CALOMENTOR_MAIL_CLIENT_SECRET,
+    refreshToken: process.env.CALOMENTOR_MAIL_REFRESH_TOKEN,
+    accessToken: process.env.CALOMENTOR_MAIL_ACCESS_TOKEN,
+    expires: "3599",
+  },
+} as TransportOptions;
+
 export const sendEmail = async (to: string, subject: string, html: string) => {
-  const transporter = createTransport({
-    // @ts-ignore: Unreachable code error
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      type: "OAuth2",
-      user: process.env.CALOMENTOR_MAIL,
-      clientId: process.env.CALOMENTOR_MAIL_CLIENT_ID,
-      clientSecret: process.env.CALOMENTOR_MAIL_CLIENT_SECRET,
-      refreshToken: process.env.CALOMENTOR_MAIL_REFRESH_TOKEN,
-      accessToken: process.env.CALOMENTOR_MAIL_ACCESS_TOKEN,
-      expires: "3599",
-    },
-  });
+  const transporter = createTransport(config);
 
   const options = {
     from: `"Calomentor" <frontendcafe@gmail.com>`,
