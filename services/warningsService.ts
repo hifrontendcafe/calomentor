d1089577b378806649e12f233887348c7c0066b8
francisco.peralta <fran.mper@gmail.com>
2022-02-12 11:10:12 -0300

    refactor forgive warning

--- services/warningsService.ts
+++ services/warningsService.ts
@@ -1,22 +1,16 @@
-import { APIGatewayProxyHandler, Callback, Context } from "aws-lambda";
-import {
-  RESPONSE_CODES,
-  STATUS,
-  TABLE_NAME_MENTORSHIP,
-  TABLE_NAME_WARNINGS,
-  WARNSTATE,
-  WARN,
-} from "../constants";
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { STATUS, WARNSTATE, WARN } from "../constants";
 import { v4 as uuidv4 } from "uuid";
-import { throwResponse } from "../utils/throwResponse";
 import { Warning } from "../types";
-import { addWarning, getWarningsData } from "../repository/warning";
+import {
+  addWarning,
+  getWarningsData,
+  updateWarning,
+} from "../repository/warning";
 import { updateMentorship } from "../repository/mentorship";
 import { makeErrorResponse, makeSuccessResponse } from "../utils/makeResponses";
 
 const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const addWarningService: APIGatewayProxyHandler = async (event) => {
   const { mentee_id, warn_type, warn_cause, mentorship_id, warning_author_id } =
@@ -74,64 +68,38 @@ export const getWarnings: APIGatewayProxyHandler = async (event) => {
 
 export const getAllWarnings: APIGatewayProxyHandler = async () => {
   try {
-    const warnings = await getWarningsData()
+    const warnings = await getWarningsData();
     return makeSuccessResponse({ warnings: warnings.Items }, "302");
   } catch (error) {
     return makeErrorResponse(400, "-303", error);
   }
 };
 
-export const forgiveWarning = async (
-  event: any,
-  context: Context,
-  callback: Callback<any>
-): Promise<void> => {
+export const forgiveWarning: APIGatewayProxyHandler = async (event) => {
   const { forgive_cause } = JSON.parse(event.body);
-  if (!event.pathParameters.id) {
-    const responseCode: keyof typeof RESPONSE_CODES = "-304";
-    return throwResponse(callback, RESPONSE_CODES[responseCode], 400, {
-      responseMessage: RESPONSE_CODES[responseCode],
-      responseCode,
-    });
+  const { id } = event.pathParameters;
+  if (!id) {
+    return makeErrorResponse(400, "-304");
   }
 
-  const params = {
-    TableName: TABLE_NAME_WARNINGS,
-    Key: { id: event.pathParameters.id },
-    ExpressionAttributeValues: {
-      ":status": WARNSTATE.FORGIVE,
-      ":forgive_cause": forgive_cause,
-    },
-    UpdateExpression: "SET forgive_cause = :forgive_cause, status = :status",
-    ReturnValues: "ALL_NEW",
-  };
-
   try {
-    const warningUpdate = await dynamoDb.update(params).promise();
-    const paramsUpdate = {
-      TableName: TABLE_NAME_MENTORSHIP,
-      Key: { id: warningUpdate.mentorship_id },
-      ExpressionAttributeValues: {
-        ":mentorship_status": STATUS.CONFIRMED,
-        ":warning_info": warningUpdate.Attributes,
+    const warningUpdate = await updateWarning(
+      id,
+      { status: WARNSTATE.FORGIVE, forgive_cause },
+      ["forgive_cause", "status"]
+    );
+
+    await updateMentorship(
+      warningUpdate.Attributes.mentorship_id,
+      {
+        mentorship_status: STATUS.CONFIRMED,
+        warning_info: warningUpdate.Attributes,
       },
-      UpdateExpression:
-        "SET mentorship_status = :mentorship_status, warning_info = :warning_info",
-      ReturnValues: "ALL_NEW",
-    };
-    await dynamoDb.update(paramsUpdate).promise();
-    const responseCode: keyof typeof RESPONSE_CODES = "303";
-    return throwResponse(callback, RESPONSE_CODES[responseCode], 200, {
-      responseMessage: RESPONSE_CODES[responseCode],
-      responseCode,
-      warningUpdate,
-    });
+      ["mentorship_status", "warning_info"]
+    );
+
+    return makeSuccessResponse(warningUpdate, "303");
   } catch (error) {
-    const responseCode: keyof typeof RESPONSE_CODES = "-305";
-    return throwResponse(callback, RESPONSE_CODES[responseCode], 400, {
-      responseMessage: RESPONSE_CODES[responseCode],
-      responseCode,
-      error,
-    });
+    return makeErrorResponse(400, "-305", error);
   }
 };
