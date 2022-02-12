82851a89ae8f8e45a830a8fe8258f7a663b0cf5f
francisco.peralta <fran.mper@gmail.com>
2022-02-12 17:43:08 -0300

    add timeslots validations

--- constants/index.ts
+++ constants/index.ts
@@ -44,6 +44,8 @@ export const RESPONSE_CODES = {
   "-111": "The mentorship is not confirmed",
   "-112": "The mentorship already has a feedback",
   "-113": "Bad Request: user_id, date y slots are required",
+  "-114": "Bad Request: date is past",
+  "-115": "Bad Request: already has a timeslot with this date or has another slot in the next 45 minutes.",
   "-200": "Unable to create user. User already exists.",
   "-201": "Unable to create user.",
   "-202": "There is no mentors",
--- utils/dates.ts
+++ utils/dates.ts
@@ -1,4 +1,5 @@
 import { zonedTimeToUtc } from "date-fns-tz";
+import * as dayjs from 'dayjs'
 
 const timeZone: string = "America/Montevideo";
 const locale: string = "es-AR";
@@ -17,4 +18,16 @@ export const toTimeString: (date: Date) => string = (date) => {
     hour: "2-digit",
     minute: "2-digit",
   });
+};
+
+export const isPastDate = (date: Date, dateToCompare?: Date): boolean => {
+  return dayjs(dateToCompare).isAfter(dayjs(date))
+};
+
+export const isSameDate = (dateOne: Date, dateTwo: Date): boolean => {
+  return dayjs(dateOne).isSame(dayjs(dateTwo))
+};
+
+export const addTime = (date: Date, timeToAdd: number, unit: dayjs.UnitType): Date => {
+  return dayjs(date).add(timeToAdd, unit).toDate()
 };
