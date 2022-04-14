b9aff13fb51ba7dc0be905fd0dc936ea2245c3d6
Francisco Peralta <fran.mper@gmail.com>
2022-04-14 15:27:19 -0300

    add warnings quantity

--- services/warningsService.ts
+++ services/warningsService.ts
@@ -152,7 +152,10 @@ export const getWarnings: APIGatewayProxyHandler = async (event) => {
       return makeSuccessResponse(null, "301");
     }
     if (allWarnings) {
-      return makeSuccessResponse(warnings.Items, "302");
+      return makeSuccessResponse(
+        { warnings_data: warnings.Items, warning_quantity: warnings.Count },
+        "302"
+      );
     }
     return makeErrorResponse(400, "-302", warnings.Items);
   } catch (error) {
